a4dd4377cc7664a8b3a09b48aaf716d669efe0ce
Ties Voskamp <ties@voskamp.nu>
2024-11-29 19:14:53 +0100

    Update switch_gateway.sh

--- src/keepalived/switch_gateway.sh
+++ src/keepalived/switch_gateway.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 # Check if an interface is provided as a parameter
 if [ -z "$1" ]; then
-    echo "Usage: $0 <interface>"
+    echo "Usage: $0 <interface> <ip-address>"
     exit 2
 fi
 
 INTERFACE="$1"
-echo "Interface: ${INTERFACE}"
-
 GATEWAY=`grep "routers" /var/lib/dhcp/dhclient.${INTERFACE}.leases | tail -1 | awk {'print $3'} | cut -f1 -d";" `
-echo "Gateway: ${GATEWAY}"
+echo "Interface ${INTERFACE} has ${GATEWAY} as gateway."
 
 ip route del default 2>/dev/null
 ip route add default via ${GATEWAY} dev ${INTERFACE}
